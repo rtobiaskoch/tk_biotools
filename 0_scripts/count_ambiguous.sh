97627d608eb39b7a205fe1944c840985c3a599e6
Robert Koch <r.tobiaskoch@gmail.com>
2024-09-14 13:34:19 -0600

    before removing echo checks

--- 0_scripts/count_ambiguous.sh
+++ 0_scripts/count_ambiguous.sh
@@ -13,6 +13,17 @@ if [ ! -f "$fasta_file" ]; then
     exit 1
 fi
 
+
+# Assign the folder path to a variable
+folder_path="2_mid"
+
+# Check if the mid folder exists
+if [ ! -d "$folder_path" ]; then
+    # If the folder does not exist, create it
+    mkdir -p "$folder_path"
+    echo "Directory created: $folder_path"
+fi
+
 #check if fasta file 
 if [[ ! $fasta_file =~ \.fasta$|\.fa$ ]] ; then
 	echo "file is not a fasta silly goose!"
@@ -26,22 +37,34 @@ fi
 #adds # to the beginning and end of each '>'| removes all new lines | #adds new line | removes empty lin
 fasta_clean=$(sed -e 's/\(^>.*$\)/#\1#/' "$fasta_file" | tr -d '\n' | sed -e 's/#/\n/g' | grep -v '^$')
 
-echo "fasta file output of the fasta_clean step below:"
-printf "%s\n" "$fasta_clean"
-echo -e "\n"
+#echo "fasta file output of the fasta_clean step below:"
+#printf "%s\n" "$fasta_clean"
+#echo -e "\n"
 
 printf "%s\n" "$fasta_clean" > 2_mid/clean.fasta
 #echo -e $fasta_clean > 2_mid/clean.fasta
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#GET SEQ NAMES
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#keep only the sequence names which start with ">" then remove the >
+seq_name=$(grep '^>' $fasta_file | sed 's/>//g')
+
+printf "%s\n" "$seq_name" > 2_mid/seq_name.txt 
+
+seq_num=$(cat 2_mid/seq_name.txt|wc -l)
+echo "Processing file with $seq_num sequences..."
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #COUNT ALL CALLS
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-echo "output of all nucleotide calls per sample:"
+#echo "output of all nucleotide calls per sample:"
 all_count=$(printf "%s\n" "$fasta_clean" | awk '!/^>/ { print length($0) }') 
-printf "%s\n" "$all_count"
+#printf "%s\n" "$all_count"
 printf "%s\n" "$all_count" > 2_mid/all_count.txt
-echo -e "\n"
+#echo -e "\n"
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -52,9 +75,9 @@ echo -e "\n"
 ATCG_fasta=$(echo "$fasta_clean" | sed '/^>/!s/[^ATCG]//gi')
 
 #check your output 
-echo "fasta file output of removing ambiguous characters:"
-printf "%s\n" "$ATCG_fasta"
-echo -e "\n"
+#echo "fasta file output of removing ambiguous characters:"
+#printf "%s\n" "$ATCG_fasta"
+#echo -e "\n"
 
 #save output
 printf "%s\n" "$ATCG_fasta" > 2_mid/atcg.fasta
@@ -64,31 +87,40 @@ printf "%s\n" "$ATCG_fasta" > 2_mid/atcg.fasta
 #GET NONAMBIG COUNTS
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #get counts
-echo "Count of all nonambiguous nucleotide calls per sample:"
+#echo "Count of all nonambiguous nucleotide calls per sample:"
+
 #count the characters in each line that doesn't start with '>'
 nonambig_count=$(printf "%s\n" "$ATCG_fasta" | awk '!/^>/ { print length($0) }')
-printf "%s\n" "$nonambig_count" #print to console
+#printf "%s\n" "$nonambig_count" #print to console
 printf "%s\n" "$nonambig_count" > 2_mid/nonambig_count.txt #save for csv
-echo -e "\n"
+#echo -e "\n"
 
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#GET SEQ NAMES
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-file_name=$(grep '^>' $fasta_file | sed 's/>//g')
 
-printf "%s\n" "$file_name"
-printf "%s\n" "$file_name" > 2_mid/file_name.txt 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #COMBINE DATA AND OUTPUT
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+fasta_file2="${fasta_file##*/}" #keep everything after the last "/"
+fasta_file2="${fasta_file2%.*}" #remove everthing last the last "." to remove the file type
 
-echo "sample,nonambig,all" > 3_output/output.csv
-paste -d ',' 2_mid/file_name.txt 2_mid/nonambig_count.txt 2_mid/all_count.txt >> 3_output/output.csv
+outfile=$(echo "3_output/"$fasta_file2"_output.csv")
+
+echo "sample,nonambig,all" > "$outfile"
+paste -d ',' 2_mid/seq_name.txt 2_mid/nonambig_count.txt 2_mid/all_count.txt >> "$outfile"
 
 
+#check if output file exists
+if [ -f $outfile ]; then
 
+filesize=$(du -h "$outfile" | cut -f 1)
+echo -e "File $outfile with size $filesize exported successfully"
+
+else 
+
+echo "$outfile doesn't exist check your bash file'"
+
+fi
 
 
 
