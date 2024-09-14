0821e4accb9d0ff6706061db02b92633a34f41db
Robert Koch <r.tobiaskoch@gmail.com>
2024-09-14 12:28:46 -0600

    output csv working

--- 0_scripts/count_ambiguous.sh
+++ 0_scripts/count_ambiguous.sh
@@ -26,43 +26,77 @@ fi
 #adds # to the beginning and end of each '>'| removes all new lines | #adds new line | removes empty lin
 fasta_clean=$(sed -e 's/\(^>.*$\)/#\1#/' "$fasta_file" | tr -d '\n' | sed -e 's/#/\n/g' | grep -v '^$')
 
-echo "output of the fasta_clean step below:"
+echo "fasta file output of the fasta_clean step below:"
 printf "%s\n" "$fasta_clean"
+echo -e "\n"
 
 printf "%s\n" "$fasta_clean" > 2_mid/clean.fasta
 #echo -e $fasta_clean > 2_mid/clean.fasta
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#Get total count of all characters
+#COUNT ALL CALLS
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 echo "output of all nucleotide calls per sample:"
-printf "%s\n" "$fasta_clean" | awk '!/^>/ { print length($0) }' 
-
+all_count=$(printf "%s\n" "$fasta_clean" | awk '!/^>/ { print length($0) }') 
+printf "%s\n" "$all_count"
+printf "%s\n" "$all_count" > 2_mid/all_count.txt
+echo -e "\n"
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#keep nonambiguous nucleotides and count them
+#NONAMBIG CALLS
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-ATCG_fasta=$(echo "$fasta_clean" | sed '/^>/!s/[^ATCG]//g')
 
+#remove the ambiguous characters by only keeping ATCG in lines that arent in the headers
+ATCG_fasta=$(echo "$fasta_clean" | sed '/^>/!s/[^ATCG]//gi')
+
+#check your output 
+echo "fasta file output of removing ambiguous characters:"
+printf "%s\n" "$ATCG_fasta"
+echo -e "\n"
+
+#save output
 printf "%s\n" "$ATCG_fasta" > 2_mid/atcg.fasta
 
-echo "output of all nonambiguous nucleotide calls per sample:"
-printf "%s\n" "$ATCG_fasta" | awk '!/^>/ { print length($0) }' 
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#GET NONAMBIG COUNTS
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#get counts
+echo "Count of all nonambiguous nucleotide calls per sample:"
+#count the characters in each line that doesn't start with '>'
+nonambig_count=$(printf "%s\n" "$ATCG_fasta" | awk '!/^>/ { print length($0) }')
+printf "%s\n" "$nonambig_count" #print to console
+printf "%s\n" "$nonambig_count" > 2_mid/nonambig_count.txt #save for csv
+echo -e "\n"
 
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#calculate nonambiguous/total to get percent nonambiguous coverage
+#GET SEQ NAMES
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+file_name=$(grep '^>' $fasta_file | sed 's/>//g')
+
+printf "%s\n" "$file_name"
+printf "%s\n" "$file_name" > 2_mid/file_name.txt 
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#COMBINE DATA AND OUTPUT
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-
-#echo -e "$ATCG_fasta" | sed -n '/^>/!{p;='}
-
-#need to make it so it doesn't remove the headers
-#potentially change so that it only keeps the ATCG instead of the ambiguous calls as that is more complicated
+echo "sample,nonambig,all" > 3_output/output.csv
+paste -d ',' 2_mid/file_name.txt 2_mid/nonambig_count.txt 2_mid/all_count.txt >> 3_output/output.csv
 
 
-# Define a list of ambiguous nucleotide characters
-#ambiguous_chars="NRYSWKMBDHV"
-#cleaned_fasta=$(echo "$fasta_clean" | sed '/^>/!s/['"$ambiguous_chars"']//g')
+
+
+
+
+
+
+
+
+
+
+
+
